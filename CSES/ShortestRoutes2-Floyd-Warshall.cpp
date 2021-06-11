d97649bc991dc2d627fad2553b1647e32513db55
Luis <69769019+Matschbirne2003@users.noreply.github.com>
2021-06-11 18:42:32 +0200

    Added Comments to ShortestRoute2

--- CSES/ShortestRoutes2-Floyd-Warshall.cpp
+++ CSES/ShortestRoutes2-Floyd-Warshall.cpp
@@ -17,7 +17,7 @@ int main()
 {
     cin >> n >> m >> q;
 
-    for(int i = 1; i<=n; i++)
+    for(int i = 1; i<=n; i++)   //Making every edge between two nodes weigth to the max value.
     {
         for(int j = 1; j<=n; j++)
         {
@@ -25,9 +25,9 @@ int main()
             graph[j][i] = MAX_INT;
         }
     }
-    for(int i = 1; i<=n; i++) graph[i][i] = 0;
+    for(int i = 1; i<=n; i++) graph[i][i] = 0; //from any node to the same node weight is one.
     ll a, b, w;
-    for(int i = 0; i<m; i++)
+    for(int i = 0; i<m; i++)    //adding the weigth given by the input between node a and b with weigth w.
     {
         cin >> a >> b >> w;
         graph[a][b] = min(graph[a][b], w);
@@ -40,7 +40,7 @@ int main()
         {
             for(int j = 1; j<=n; j++)
             {
-                graph[i][j] = min(graph[i][j], graph[i][relax]+graph[relax][j]);
+                graph[i][j] = min(graph[i][j], graph[i][relax]+graph[relax][j]);    //Floyd-Warshall Algorithm where we try to find a shorter way by going over another node k to the destination node.
             }
         }
     }
@@ -48,12 +48,12 @@ int main()
     for(int i = 0; i<q; i++)
     {
         cin >> a >> b;
-        if(graph[a][b] == MAX_INT)
+        if(graph[a][b] == MAX_INT)  //if the weight hasnt changed from the max value there is no possible path so -1
         {
             cout << -1 << endl;
             continue;
         }
-        cout << graph[a][b] << endl;
+        cout << graph[a][b] << endl;    //otherwise print the value listed in the 2D-Matrix
 
     }
 }
